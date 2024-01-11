4b87a26136a1ea5f8762254e263203d4765fcce4
zachd93 <zachdebolt1993@gmail.com>
2024-01-10 16:19:27 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QPushButton
+import sys
+
+app = QApplication([])
+window = QWidget()
+window.setWindowTitle("PyQt App")
+window.setGeometry(100, 100, 280, 80)
+helloMsg = QLabel("<h1>Hello, World!</h1>", parent=window)
+helloMsg.move(60, 15)
+
+window.show()
+sys.exit(app.exec())
