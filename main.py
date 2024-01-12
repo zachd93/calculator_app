e42c63bba4b4fe0a0772d3e9d5f73659ef8f9c08
zachd93 <zachdebolt1993@gmail.com>
2024-01-12 15:02:50 -0800

    Uncompleted. Needs bug fixes and code clean up

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from PyQt6.QtWidgets import QApplication, QGridLayout, QLineEdit, QMainWindow, QPushButton, QVBoxLayout, QWidget
 from PyQt6.QtCore import Qt
 import sys
+from functools import partial
 
+ERROR_MSG = "ERROR"
 WINDOW_SIZE = 235
 DISPLAY_HEIGHT = 35
 BUTTON_SIZE = 40
@@ -23,7 +25,7 @@ class PyCalcWindow(QMainWindow):
         self.display = QLineEdit()
         self.display.setFixedHeight(DISPLAY_HEIGHT)
         self.display.setAlignment(Qt.AlignmentFlag.AlignRight)
-        self.display.setReadOnly(True)
+        self.display.setReadOnly(False)
         self.generalLayout.addWidget(self.display)
 
     def _createButtons(self):
@@ -44,11 +46,60 @@ class PyCalcWindow(QMainWindow):
 
         self.generalLayout.addLayout(buttonsLayout)
 
+    def setDisplayText(self, text):
+        """Set the display's text."""
+        self.display.setText(text)
+        self.display.setFocus()
+
+    def displayText(self):
+        """Get the display's text."""
+        return self.display.text()
+
+    def clearDisplay(self):
+        """Clear the display"""
+        self.setDisplayText("")
+
+    def evaluateExpression(expression):
+        try:
+            result = str(eval(expression, {}, {}))
+        except Exception:
+            result = ERROR_MSG
+            return result
+
+
+class PyCalc:
+    """PyCalc controller class"""
+    def __init__(self, model, view):
+        self._evaluate = model
+        self._view = view
+        self._connectSignalsAndSlots()
+
+    def _calculateResult(self):
+        result = self._evaluate(expression=self._view.displayText())
+        self._view.setDisplayText(result)
+
+    def _buildExpression(self, subExpression):
+        if self._view.displayText() == ERROR_MSG:
+            self._view.clearDisplay()
+        expression = self._view.displayText() + subExpression
+        self._view.setDisplayText(expression)
+
+    def _connectSignalsAndSlots(self):
+        for keySymbol, button in self._view.buttonMap.items():
+            if keySymbol not in {"=", "C"}:
+                button.clicked.connect(
+                    partial(self._buildExpression, keySymbol)
+                )
+        self._view.buttonMap["="].clicked.connect(self._calculateResult)
+        self._view.display.returnPressed.connect(self._calculateResult)
+        self._view.buttonMap["C"].clicked.connect(self._view.clearDisplay)
+
 
 def main():
     pycalcApp = QApplication([])
     pycalcWindow = PyCalcWindow()
     pycalcWindow.show()
+    PyCalc(model=pycalcWindow.evaluateExpression, view=pycalcWindow)
     sys.exit(pycalcApp.exec())
 
 
